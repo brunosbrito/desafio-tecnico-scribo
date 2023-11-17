01ddf8115307775917b6134329b68f82f062d044
brunosbrito <brunobrito728@gmail.com>
2023-11-17 19:35:45 -0300

    feat: criado a funcao de somar todos os números  divisíveis por 3 e 5

---
+++ desafio/script.js
@@ -0,0 +1,21 @@
+const readlineSync = require('readline-sync');
+
+function main(valorDigitado) {
+  let soma = 0;
+
+  for (let i = 1; i < valorDigitado; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      soma += i;
+    }
+  }
+
+  console.log(`A soma dos números menores que ${valorDigitado} e divisíveis por 3 ou 5 é: ${soma}`);
+  return soma;
+}
+
+if (require.main === module) {
+  const valorDigitado = parseFloat(readlineSync.question('Digite um numero: '));
+  main(valorDigitado);
+}
+
+module.exports = { main };
