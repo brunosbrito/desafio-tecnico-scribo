1355d2559881efff243e283417df68f9753a199d
brunosbrito <brunobrito728@gmail.com>
2023-11-17 19:57:26 -0300

    feat: adiciona validacao para verificar se e um numero

--- desafio/script.js
+++ desafio/script.js
@@ -3,6 +3,11 @@ const readlineSync = require('readline-sync');
 function main(valorDigitado) {
   let soma = 0;
 
+  if(isNaN(valorDigitado)) {
+    console.log('Por favor digite um numero')
+    return null
+  }
+
   for (let i = 1; i < valorDigitado; i += 1) {
     if (i % 3 === 0 || i % 5 === 0) {
       soma += i;
